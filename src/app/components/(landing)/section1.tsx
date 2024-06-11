20d7755afd2491abc243b3ddc6e2675a1463419c
Thiago Cerutti <thiagocerutti@gmail.com>
2024-06-11 18:38:22 -0300

    ajustes visuais

--- src/app/components/(landing)/section1.tsx
+++ src/app/components/(landing)/section1.tsx
@@ -2,22 +2,17 @@ import { Header } from "../header";
 import Button from "../button";
 import Image from "next/image";
 import { CiLinkedin } from "react-icons/ci";
+import './section1.css'
 
 export default function Section1(){
     return (
-        <div className='h-screen relative '>
+        <div className="imagemDeFundo" >
             <Header/>
-            <CiLinkedin />
-            <Image src='/BG.png' className='w-full h-full top-0 left-0' width={1600} height={1600} alt={""}></Image>
-
-            <div className='absolute bg-transparent flex h-full items-center justify-center paq-imae'>
-                <div className='flex flex-col justify-center items-center gap-3 '>
                     <h1 className='text-5xl max-w-64 items-center'>O futuro da tecnologia Começa aqui.</h1>
                     <p className=''>Desde 2018, o PAQ tem trabalhado incansavelmente para impulsionar o conhecimento e as oportunidades no mundo da tecnologia.</p>
                     <Button title="DOE PARA O PAQ" href="/" />
-                </div>
+
             </div>
-        </div>
     )
 }
 
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,21 +4,15 @@ import { CiLinkedin } from "react-icons/ci";
 
 export function Header(){
     return(
-        <div className='flex w-full items-center justify-between px-3 bg-transparent absolute'>
-            <div>
-                <Image src='/logo-paq.png' width={45}
-              height={45} alt={""}></Image>
-            </div>
-            <div className='flex gap-6 items center'>
-                <Link href={""}>sobre nós</Link>
-                <Link href={""}>como fazer parte</Link>
-                <Link href={""}>empresas</Link>
-                <Link href={""}>frentes</Link>
-            </div>
-            <div className="flex gap-6 items-center">
-                <CiLinkedin />
-                <Link href={""}>ln</Link>
-            </div>
+        <div className='paqNavbar'>
+            <Image src={'/Logo-paq.png'} width={45} height={45} alt={""}></Image>
+            <CiLinkedin className="iconeRedesSociais" />
+            <Link href={""}>sobre nós</Link>
+            <Link href={""}>como fazer parte</Link>
+            <Link href={""}>empresas</Link>
+            <Link href={""}>frentes</Link>
+            <CiLinkedin />
+            <Link href={""}>ln</Link>
         </div>
     )
 }
