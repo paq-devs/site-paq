2cd45b282599cfcd9c52498493edffc932c3441a
Thiago Cerutti <thiago.cerutti@rdstation.com>
2024-06-20 17:41:19 -0300

    adicionado componente da seção inicial

--- src/app/(Site)/(LandingPage)/page.tsx
+++ src/app/(Site)/(LandingPage)/page.tsx
@@ -1,11 +1,13 @@
 import PaqHeader from "@/app/components/(landing)/PaqHeader";
 import ConectarETransformar from "@/app/components/(landing)/ConectarETransformar";
+import SecaoInicial from "@/app/components/SecaoInicial";
 
 export default function Home() {
   return (
     <>
       <PaqHeader />
       <ConectarETransformar />
+      <SecaoInicial />
     </>
   );
 }
