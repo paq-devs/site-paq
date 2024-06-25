79cc2f26ec2d68619aae648bc99065b7e8130791
Thiago Cerutti <thiagocerutti@gmail.com>
2024-06-25 19:01:24 -0300

    Criação da seção "Perguntas Frequentes" (#6)
    * chore: install packages
    
    * chore: altera estilo global
    
    * Fix: Importação de componente

--- src/app/(Site)/(LandingPage)/page.tsx
+++ src/app/(Site)/(LandingPage)/page.tsx
@@ -1,11 +1,13 @@
 import PaqHeader from "@/app/components/(landing)/PaqHeader";
 import ConectarETransformar from "@/app/components/(landing)/ConectarETransformar";
+import PaqFaq from "@/app/components/PaqFaq";
 
 export default function Home() {
   return (
     <>
       <PaqHeader />
       <ConectarETransformar />
+      <PaqFaq />
     </>
   );
 }
