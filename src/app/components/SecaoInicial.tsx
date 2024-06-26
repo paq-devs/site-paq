a036e792f46a6d624d2da435a3d920c05b03216e
iofme <jgempregos02@gmail.com>
2024-06-26 10:13:49 -0300

    Animações finlizadas

--- src/app/components/SecaoInicial.tsx
+++ src/app/components/SecaoInicial.tsx
@@ -16,9 +16,17 @@ export default function SecaoInicial(){
           <p className="quadro2_text2"></p>
           <div className="quadro2-img1"></div>
         </div>
+          
+        <div className="section-inicial_quadro">
+          <div className="quadro3-img2"></div>
+          <p className="quadro3_text3"></p>
+        </div>
         
-        
-      
+        <div className="section-inicial_quadro">
+          <p className="quadro4_text4"></p>
+          <div className="quadro4-img3"></div>
+          <div className="quadro4-img4"></div>
+        </div>
       </section>
     </>
   );
