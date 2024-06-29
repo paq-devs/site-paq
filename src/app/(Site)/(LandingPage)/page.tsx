071be8aff37003089456ee28502a714cc5b92b3c
João-Kuhn <117414638+iofme@users.noreply.github.com>
2024-06-29 13:16:38 -0300

    Criação da seção "Um futuro diverso começa aqui"  (#5)
    * criando a branch
    
    * analizar bug
    
    * Primeiras animações
    
    * Configurando variaveis do tailwind
    
    * Criado mais variaveis no tailwind e utilização dessas variaveis
    
    * feat: exemplo de imageBackground no .css
    
    * Iniciando a troca de textos com movimento
    
    * testando alguns valores para a animação
    
    * adicionado componente da seção inicial
    
    * animação e pausas criadas
    
    * Arrumando os espaços (terminar em casa)
    
    * Terminar em casa
    
    * Animações finlizadas
    
    * Fontes adicionadas
    
    * Animação finalizada
    
    * Finalizado a sessão-inicial
    
    * fix: clean up
    
    * fix: renomeia imagens
    
    * fix: estrutura dos componentes
    
    * fix: nome do componente
    
    ---------
    
    Co-authored-by: Thiago Cerutti <thiago.cerutti@rdstation.com>
    Co-authored-by: Thiago Cerutti <thiagocerutti@gmail.com>

--- src/app/(Site)/(LandingPage)/page.tsx
+++ src/app/(Site)/(LandingPage)/page.tsx
@@ -1,5 +1,6 @@
 import PaqHeader from "@/app/components/(landing)/PaqHeader";
 import ConectarETransformar from "@/app/components/(landing)/ConectarETransformar";
+import UmFuturoInovadorDiversoComecaAqui from "@/app/components/UmFuturoInovadorDiversoComecaAqui";
 import PaqFaq from "@/app/components/PaqFaq";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <PaqHeader />
       <ConectarETransformar />
+      <UmFuturoInovadorDiversoComecaAqui />
       <PaqFaq />
     </>
   );
