d72ddb627632b29c77e55109d79995c667fee9d0
Thiago Cerutti <thiagocerutti@gmail.com>
2024-07-02 20:21:07 -0300

    Estilos para o componente pai

--- src/app/components/PapelPAQ.tsx
+++ src/app/components/PapelPAQ.tsx
@@ -1,7 +1,21 @@
-import { link } from "fs"
-import Image from "next/image"
+"use client";
+import { link } from "fs";
+import Image from "next/image";
 import CardsPapelPaq from "./CardsPapelPaq";
-import joinha from "./joinha.svg"
+import joinha from "./joinha.svg";
+import styled from "styled-components";
+
+const ListaDeCards = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: -12px;
+  overflow-x: scroll;
+
+  :nth-child(even) {
+    rotate: -5deg;
+  }
+`;
 
 export default async function PapelPAQ() {
   // const response = await fetch('http://localhost:5000/cards')
@@ -44,7 +58,7 @@ export default async function PapelPAQ() {
         O PAQ fortalece a jornada de quem tá com a gente através desses pilares:
       </p>
 
-      <ul className="papelpaq-lista">
+      <ListaDeCards>
         {data.map((item: any) => (
           <CardsPapelPaq
             key={item.id}
@@ -53,17 +67,7 @@ export default async function PapelPAQ() {
             texto={item.texto}
           />
         ))}
-        {/* {data.map((iten:any) => <li className="papel-lista-cards" key={iten.id}>
-                    <Image
-                    src={iten.icone}
-                    width={0}
-                    height={0}
-                    alt=""
-                    />
-                    <h3>{iten.titulo}</h3>
-                    <p>{iten.texto}</p>
-                </li>)} */}
-      </ul>
+      </ListaDeCards>
     </section>
   );
 }
