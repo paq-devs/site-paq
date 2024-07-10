3f8cad1575a6a6bc2dd8fa9f8936456eaaa05426
PedroRocha2 <pedro.rocha.neni@gmail.com>
2024-07-10 12:00:26 -0300

    correção dos espaçamentos entre as frases

--- src/app/components/Impactometro/cardImpaqtrometro.tsx
+++ src/app/components/Impactometro/cardImpaqtrometro.tsx
@@ -9,20 +9,30 @@ const CardImpaqtrometrofundo = styled.div`
     flex-shrink: 0;
     border-radius: 25px;
     background: #122329;
+    padding: 19px 0;
 
 `
 const ParteDeCima = styled.div`
     display:flex;
     flex-direction: row;
     h2 {
-        width: 116.55px;
         color: #FFF;
         text-align: right;
         font-family: "PP Formula Condensed";
         font-size: 64px;
         font-style: normal;
         font-weight: 400;
-        line-height: 94.167px; /* 147.135% */
+        line-height: 71px;
+        text-transform: uppercase;
+    }
+    h3 {
+        color: #FFF;
+        text-align: right;
+        font-family: "PP Formula Condensed";
+        font-size: 48.584px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 30px;
         text-transform: uppercase;
     }
 `
@@ -36,26 +46,31 @@ const Icones = styled.div`
     align-items: center;
     justify-content:center;
 `
+const NumeroEstilizado = styled.span`
+    color: #0EB3E8;
+`
 
-export default function CardImpaqtrometro(){
+export default function CardImpaqtrometro() {
     return (
         <div>
             <CardImpaqtrometrofundo>
                 <div>
                     <ParteDeCima>
-                    <Icones>
-                        <Image src={grupoDePessoas} alt='' width={58} height={49} />
-                    </Icones>
-                    <h2>
-                        +80 JOVENS
-                    </h2>
+                        <Icones>
+                            <Image src={grupoDePessoas} alt='' width={58} height={49} />
+                        </Icones>
+                        <div>
+                            <h2>
+                                +<NumeroEstilizado>80</NumeroEstilizado>
+                            </h2>
+                            <h3>
+                                JOVENS
+                            </h3>
+                        </div>
                     </ParteDeCima>
                 </div>
                 <div>
-
                 </div>
-                
-
             </CardImpaqtrometrofundo>
         </div>
     )
