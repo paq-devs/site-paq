892a54a96d29345a5e105575062b0ccdea9e9fbe
R0ch4011 <pedro.rocha.neni@gmail.com>
2024-07-14 02:02:51 -0300

    adição da font juntamente com alterações de classes

--- src/app/(Site)/(LandingPage)/page.tsx
+++ src/app/(Site)/(LandingPage)/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
     <>
       <PaqHeader />
       <ConectarETransformar />
-      <VideoConexao />
     </>
   );
 }
--- src/app/components/(landing)/VideoConexao.tsx
+++ src/app/components/(landing)/VideoConexao.tsx
@@ -2,15 +2,16 @@ import Image from "next/image"
 
 export default function VideoConexao() {
     return (
-        <div className="relative w-full h-[400px]">
+        <div className="relative w-full h-[300px]">
             <Image src='/ilustrações.png' layout="fill" objectFit="cover" alt="" />
-            <div className="absolute inset-0 flex justify-center items-start mt-44">
+            <div className="absolute inset-0 flex justify-center items-start mt-28 shirink-0 ">
                 <iframe 
-                    width="288" 
-                    height="162"
+                    width="330" 
+                    height="200"
                     src="https://www.youtube.com/embed/tgbNymZ7vqY"
                     frameBorder="0"
                     allowFullScreen
+                    className="rounded-md"
                 >
                 </iframe>
             </div>
