151514a96909af38643ad094546ec065491015e0
Thiago Cerutti <thiagocerutti@gmail.com>
2024-07-21 13:50:57 -0300

    Sugestões de melhoria para simplificação do código

--- src/app/(Site)/(LandingPage)/page.tsx
+++ src/app/(Site)/(LandingPage)/page.tsx
@@ -1,6 +1,5 @@
 import PaqHeader from "@/app/components/(landing)/PaqHeader";
-import ConectarETransformar from "@/app/components/(landing)/ConectarETransformar";
-import VideoConexao from "@/app/components/(landing)/VideoConexao";
+import ConectarETransformar from "@/app/components/ConectarETransformar";
 import UmFuturoInovadorDiversoComecaAqui from "@/app/components/UmFuturoInovadorDiversoComecaAqui";
 import PaqFaq from "@/app/components/PaqFaq";
 
