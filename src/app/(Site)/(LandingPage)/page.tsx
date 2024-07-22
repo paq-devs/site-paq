53224dc3c348c37e96f640ca78dbd12cb46e7fb8
PedroRocha2 <pedro.rocha.neni@gmail.com>
2024-07-22 18:43:07 -0300

    criação da sessão junco com a gente

--- src/app/(Site)/(LandingPage)/page.tsx
+++ src/app/(Site)/(LandingPage)/page.tsx
@@ -3,6 +3,7 @@ import ConectarETransformar from "@/app/components/(landing)/ConectarETransforma
 import UmFuturoInovadorDiversoComecaAqui from "@/app/components/UmFuturoInovadorDiversoComecaAqui";
 import PaqFaq from "@/app/components/PaqFaq";
 import Impaqtrometro from "@/app/components/Impactometro/Impaqtrometro";
+import JuntoComAGente from "@/app/components/JuntoComAGente/JuntoComAGente";
 
 export default function Home() {
   return (
---
+++ src/app/components/JuntoComAGente/JuntoComAGente.tsx
@@ -0,0 +1,20 @@
+"use client";
+import styled from "styled-components"
+import Image from 'next/image'
+
+const CabecalhoJuntoComAgente = styled.div `
+    display:flex;
+` 
+
+export default function JuntoComAGente(){
+    return(
+        <div>
+            <CabecalhoJuntoComAgente>
+                <div>
+                
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et d</p>
+            </CabecalhoJuntoComAgente>
+        </div>
+    )
+}
