f7ec009d4c839ad075adc31595920d78e03a305c
PedroRocha2 <pedro.rocha.neni@gmail.com>
2024-07-26 18:07:10 -0300

    criação dos cards apoiadores e parceiros

--- src/app/components/JuntoComAGente/JuntoComAGente.tsx
+++ src/app/components/JuntoComAGente/JuntoComAGente.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import coroa from './img/coroa.png'
 import './JuntoComAGente.css'
+import CardApoiadoras from './cardApoiadoras'
 import CardParceira from './cardParceiras'
+
 
 export default function JuntoComAGente(){
     return(
@@ -9,9 +11,10 @@ export default function JuntoComAGente(){
             <Image src={coroa} alt='' width={160} height={90} className='my-9'/>
             <h2 className='corpo'><span className='font-bold'>Junto com a gente</span> <br /> nessa missão!</h2>
             <p className='paragrafo'>Nossa rede de apoio é fundamental para o sucesso de nossas iniciativas. Conheça nossos parceiros que acreditam em um futuro mais inclusivo e diversificado.</p> 
-            <div className='my-16'>
-                <CardParceira />
-            </div>
+                <div className='my-10'>
+                    <CardApoiadoras />
+                    <CardParceira />
+                </div>
             </div>
     )
 }
--- src/app/components/JuntoComAGente/cardParceiras.tsx
+++ src/app/components/JuntoComAGente/cardParceiras.tsx
@@ -1,60 +1,41 @@
-import Image from "next/image";
+import acate from './img/parceiras/acate.png';
+import ambar from './img/parceiras/ambar.png';
+import bewiki from './img/parceiras/bewiki.png';
+import impacthub from './img/parceiras/impacthub.png';
+import txm from './img/parceiras/txm.png';
+import rdstation from './img/parceiras/rdstation.png';
+import atre from './img/parceiras/atre.png';
+import inaitec from './img/parceiras/inaitec.png';
 
-import betahub from './img/apoiadoras/betahub.png'
-import mercadoLivre from  './img/apoiadoras/mercadolivre.png'
-import cd2 from './img/apoiadoras/cd2.png'
-import silvaSchutz from './img/apoiadoras/silvaschutz.png'
-import h from './img/apoiadoras/h.png'
-import aqtech from './img/apoiadoras/aqtech.png'
-import weduka from  './img/apoiadoras/weduka.png'
+import Image from 'next/image'
 
-import './JuntoComAGente.css'
-
-
-
+const imagesTop = [
+    { src: acate, width: 191, height: 95 },
+    { src: inaitec, width:124, height:119},
+    { src: ambar, width: 117, height: 117 },
+    { src: bewiki, width: 156, height: 36 }, // Dimensões personalizadas para bewiki
+    { src: impacthub, width: 102, height: 102 },
+    { src: txm, width: 123, height: 60 },
+    { src: rdstation, width: 188, height: 34 },
+    { src: atre, width: 103, height: 94 },
+  ];
+  
 export default function CardParceira(){
-    const imagesTop  = [
-        { src: betahub, width: 194, height: 63 },
-        { src: mercadoLivre, width: 170, height: 43 },
-        { src: cd2, width: 100, height:  49 }, // Dimensões personalizadas para cd2
-        { src: silvaSchutz, width: 120, height: 88 },
-      ];
-
-      const imagesBottom = [
-        { src: h, width: 90, height: 90 },
-        { src: aqtech, width:  164, height:  60 },
-        { src: weduka, width: 161, height: 32 },
-      ];
-
-      return (
+    return(
     <div className="bg-fundocinza rounded-[20px] py-4">
-      <h2 className="titulocard">APOIADORES</h2>
-
-      {/* Primeira linha de imagens */}
-      <div className="grid grid-cols-4 gap-6 p-4 items-center">
-        {imagesTop.map((image, index) => (
-          <div key={index} className="w-full">
-            <Image
-              src={image.src}
-              alt={`Imagem ${index + 1}`}
-              width={image.width}
-              height={image.height}
-            />
-          </div>
-        ))}
-      </div>
-      <div className="flex justify-center gap-4 p-4 items-center">
-        {imagesBottom.map((image, index) => (
-          <div key={index} className="w-full md:w-1/4">
-            <Image
-              src={image.src}
-              alt={`Imagem ${index + 5}`}
-              width={image.width}
-              height={image.height}
-            />
-          </div>
-        ))}
-      </div>
+    <h2 className="titulocard">PARCEIRAS</h2>
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-6 p-4 items-center">
+      {imagesTop.map((image, index) => (
+        <div key={index} className="w-full">
+          <Image
+            src={image.src}
+            alt={`Imagem ${index + 1}`}
+            width={image.width}
+            height={image.height}
+          />
+        </div>
+      ))}
+    </div>
     </div>
     )
 }
