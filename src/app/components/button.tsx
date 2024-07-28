3b7247d85eec360bbc959c33fba58dbffc5bad86
João-Kuhn <117414638+iofme@users.noreply.github.com>
2024-07-28 09:48:58 -0300

    Header joao (#15)
    * terminar depois
    
    * Finalizado header novo
    
    * divisão do componente em uma pasta específica + indentação
    
    ---------
    
    Co-authored-by: Thiago Cerutti <thiagocerutti@gmail.com>

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,23 +1,21 @@
-'use-client'
+"use-client";
 
 import Link from "next/link";
+import style from "./Header/PaqHeader.module.css";
 
 type Props = {
-    title: string;
-    href: string;
-    width?: string;
-    height?: string;
+  href: string;
+  width?: string;
+  height?: string;
 };
 
-export default function Button({ title, href, width, height }: Props) {
-    return (
-        <Link 
-            href={href} 
-            className={`flex items-center justify-center rounded-[10px] bg-azul-paq text-base text-white font-semibold`}
-            type="button"
-            style={{ width: width, height: height }}
-        >
-            {title}
-        </Link>
-    );
+export default function Button({ href, width, height }: Props) {
+  return (
+    <Link
+      href={href}
+      className={style.link}
+      type="button"
+      style={{ width: width, height: height }}
+    ></Link>
+  );
 }
