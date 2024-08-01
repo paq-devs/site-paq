e3db3c4b4451682d731fb2c66914978c276f0899
Pedro Rocha <105256969+PedroRocha2@users.noreply.github.com>
2024-08-01 19:04:14 -0300

    Ajustes em todas as versões do impaqtrometro (#25)
    * Ajustes em todas as versões do impaqtrometro
    
    * alterações mentoria
    
    * ajuste de width no paragrafo
    
    * ajuste de width v2

--- src/app/components/Impactometro/cardImpaqtrometro.tsx
+++ src/app/components/Impactometro/cardImpaqtrometro.tsx
@@ -1,7 +1,5 @@
-'use-client'
-import styled from 'styled-components';
-import grupoDePessoas from './img/grupoDePessoas.png'
 import Image from 'next/image'
+import './cardImpaqtrometro.css'
 
 
 type props = {
@@ -11,95 +9,32 @@ type props = {
   descricao: string;
 };
 
-const CardImpaqtrometrofundo = styled.div`
-    width: 252px;
-    height: 215px;
-    flex-shrink: 0;
-    border-radius: 25px;
-    background: #122329;
-    padding: 19px 0;
-
-`
-const ParteDeCima = styled.div`
-    display:flex;
-    flex-direction: row;
-    justify-content: space-between;
-    h2 {
-        color: #FFF;
-        text-align: right;
-        font-family: "PP Formula Condensed";
-        font-size: 64px;
-        font-style: normal;
-        font-weight: 400;
-        line-height: 71px;
-        text-transform: uppercase;
-        margin-right: 13px;
-    }
-    h3 {
-        color: #FFF;
-        text-align: right;
-        font-family: "PP Formula Condensed";
-        font-size: 48.584px;
-        font-style: normal;
-        font-weight: 400;
-        line-height: 30px;
-        text-transform: uppercase;
-        margin-right: 13px;
-    }
-`
-const Icones = styled.div`
-    display:flex;
-    width: 120px;
-    height: 120px;
-    flex-shrink: 0;
-    background-color: #0EB3E8;
-    border-radius: 100%;
-    align-items: center;
-    justify-content:center;
-    margin-left: -10px
-`
-const NumeroEstilizado = styled.span`
-    color: #0EB3E8;
-`
-const ParteDeBaixo = styled.div`
-    color: #FFF;
-    width: 155px;
-    text-align: right;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 18px;
-    margin-left: 88px;
-
-`
-
 export default function CardImpaqtrometro({ image, numero, titulo, descricao }: props) {
     return (
         <div>
-            <CardImpaqtrometrofundo>
-                <div>
-                    <ParteDeCima>
-                        <Icones>
-                            <Image src={grupoDePessoas} alt='' width={58} height={49} />
-                        </Icones>
-                        <div>
-                            <h2>
-                                +<NumeroEstilizado>{numero}</NumeroEstilizado>
-                            </h2>
-                            <h3>
-                                {titulo}
-                            </h3>
-                        </div>
-                    </ParteDeCima>
-                    <ParteDeBaixo>
-                        <div>
-                            <p>{descricao}</p>
-                        </div>
-                    </ParteDeBaixo>
+            <div className='hidden lg:block bg-[#122329] py-8 w-[403px] h-[344px] rounded-[40px]'>
+                <div className='flex items-center gap-4'>
+                    <div className='iconeImpaqtrometro'>
+                        <Image src={image} width={92} height={79} alt='' className='aspect-auto'/>
+                    </div>
+                    <h2 className='cardImpaqtrometroTitulo'>+<span className='text-azul-paq'>{numero}</span><br/>
+                    {titulo}
+                    </h2>
                 </div>
-                <div>
+            <p className='cardImpaqtrometroTexto w-80 ml-16'>{descricao}</p>
+            </div>
+            <div className='bg-[#122329] py-8 w-[244px] h-[208px] rounded-[40px] lg:hidden'>
+                <div className='flex items-center gap-4'>
+                    <div className='iconeImpaqtrometro'>
+                        <Image src={image} width={55} height={47} alt='' className='aspect-auto'/>
+                    </div>
+                    <h2 className='cardImpaqtrometroTitulo'>+<span className='text-azul-paq'>{numero}</span><br/>
+                    {titulo}
+                    </h2>
                 </div>
-            </CardImpaqtrometrofundo>
+                <p className='cardImpaqtrometroTexto w-36 ml-20'>{descricao}</p>
+            </div>
         </div>
+       
     )
 }
--- src/app/components/Impactometro/fontPaq.tsx
+++ src/app/components/Impactometro/fontPaq.tsx
@@ -1,3 +1,4 @@
+'use-client'
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
