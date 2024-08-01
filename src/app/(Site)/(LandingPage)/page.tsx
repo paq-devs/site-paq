b709462f4cca399c163c1dd1e3defcad2773fc44
R0ch4011 <pedro.rocha.neni@gmail.com>
2024-08-01 19:43:16 -0300

    correção do UmFuturoInovadorV555

--- src/app/(Site)/(LandingPage)/page.tsx
+++ src/app/(Site)/(LandingPage)/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
   return (
     <>
       <PaqHeader />
+      <UmFuturoInovadorDiversoComecaAqui/>
       <ConectarETransformar />
       <PapelPAQ />
       <Impaqtrometro />
