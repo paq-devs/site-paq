c21ab01b277617d70199e0df3cfcf5f08358c25c
Thiago Cerutti <thiagocerutti@gmail.com>
2024-08-02 21:35:04 -0300

    fix: fonte do botão

--- src/app/components/ComoPossoApoiar/ButtonPreto.tsx
+++ src/app/components/ComoPossoApoiar/ButtonPreto.tsx
@@ -13,7 +13,7 @@ export default function ButtonPreto({ title, href, width, height }: Props) {
     return (
         <Link
             href={href}
-            className={`flex items-center justify-center rounded-[10px] bg-preto text-base text-white font-semibold`}
+            className={`font-poppins flex items-center justify-center rounded-[10px] bg-preto text-base text-white font-semibold`}
             type="button"
             style={{ width: width, height: height }}
         >
