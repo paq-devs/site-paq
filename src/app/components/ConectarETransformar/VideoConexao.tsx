6de3fd08f4a4589ac7fb5a4c8b5146c0fba6905c
iofme <jgempregos02@gmail.com>
2024-08-14 09:46:06 -0300

    Arrumando o link do vídeo

--- src/app/components/ConectarETransformar/VideoConexao.tsx
+++ src/app/components/ConectarETransformar/VideoConexao.tsx
@@ -5,7 +5,7 @@ export default function VideoConexao() {
   return (
     <div className={styles.videoContainer}>
       <iframe
-        src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        src="https://www.youtube.com/embed/9y1dih3eeJA?si=NO0ahc6Gf2caZ_gf"
         allowFullScreen
         className="rounded-md"
       ></iframe>
