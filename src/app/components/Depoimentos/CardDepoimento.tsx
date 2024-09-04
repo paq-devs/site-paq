2188430bacb5f96869b424074928463e96d371e1
Thiago Cerutti <thiagocerutti@gmail.com>
2024-09-03 22:11:31 -0300

    fix: padronização do tamanho dos cards

--- src/app/components/Depoimentos/CardDepoimento.tsx
+++ src/app/components/Depoimentos/CardDepoimento.tsx
@@ -1,31 +1,20 @@
-"use client"
+"use client";
 import style from "./CardDepoimento.module.css";
 import Image from "next/image";
 
-
-
-export default function CardDepoimento({
-  id,
-  pathFoto,
-  text,
-  name,
-  role,
-}: any) {
-
+export default function CardDepoimento({ id, pathFoto, text, name, role }: any) {
   return (
-    <>
-      <div className={style.card} key={id}>
-        <figure>
-          <Image src={pathFoto} alt="" className={style.card_image} />
-        </figure>
-        <article className={style.descricao}>
-          <p className={style.texto}>{text}</p>
-          <div>
-            <h3 className={style.nome}>{name}</h3>
-            <p className={style.cargo}>{role}</p>
-          </div>
-        </article>
-      </div>
-    </>
+    <div className={style.card} key={id}>
+      <figure>
+        <Image src={pathFoto} alt="" className={style.card_image} />
+      </figure>
+      <article className={style.descricao}>
+        <p className={style.texto}>{text}</p>
+        <div>
+          <h3 className={style.nome}>{name}</h3>
+          <p className={style.cargo}>{role}</p>
+        </div>
+      </article>
+    </div>
   );
 }
