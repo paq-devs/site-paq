1fb7e657943e858e1711b19c848aa09b763a2885
João-Kuhn <117414638+iofme@users.noreply.github.com>
2024-09-09 21:17:31 -0300

    Finalizado carrossel (#36)
    * Iniciando Sessão Depoimentos
    
    * Estruturação do componente card
    
    Co-authored-by: Pedro Rocha <PedroRocha2@users.noreply.github.com>
    Co-authored-by: João-Kuhn <iofme@users.noreply.github.com>
    
    * fix: package-lock.json
    
    * Criando a Banch
    
    * Continuar na unidade
    
    * fix: estrutura para carregar imagens dos depoimentos
    
    * buscando imagens
    
    * Continuar no paq
    
    * Estados funcionando
    Co-authored-by: Thiago Cerutti <thiagocerutti@gmail.com>
    Co-authored-by: Pedro Rocha <PedroRocha2@users.noreply.github.com>
    
    * inserindo swiperjs
    
    * instalando sharp para imagem
    
    * adicionando a propriedade key
    
    * Continuar na unidade
    
    * Revisar como trocar o icone do swiper
    
    * feat: Adding new css to centralize cards
    
    * fix: padronização do tamanho dos cards
    
    * Finalizado cards
    
    * Fix: habilitando todas as seções
    
    * fix: estilos do carrossel
    
    * Arrumando carrosel
    
    * Correções dimensões
    
    * Arrumando ordem
    
    ---------
    
    Co-authored-by: Thiago Cerutti <thiagocerutti@gmail.com>
    Co-authored-by: Pedro Rocha <PedroRocha2@users.noreply.github.com>
    Co-authored-by: João-Kuhn <iofme@users.noreply.github.com>
    Co-authored-by: Felipe Flores <felipebruckmann@hotmail.com.br>

--- src/app/(Site)/(LandingPage)/page.tsx
+++ src/app/(Site)/(LandingPage)/page.tsx
@@ -1,8 +1,8 @@
 import PaqHeader from "@/app/components/Header/PaqHeader";
 import ConectarETransformar from "@/app/components/ConectarETransformar";
 import UmFuturoInovadorDiversoComecaAqui from "@/app/components/UmFuturoInovadorDiversoComecaAqui";
-import PaqFaq from "@/app/components/PaqFaq";
 import Impaqtrometro from "@/app/components/Impactometro/Impaqtrometro";
+import Depoimentos from "@/app/components/Depoimentos/Depoimentos"
 import PapelPAQ from "@/app/components/PapelPaq/PapelPAQ";
 import JuntoComAGente from "@/app/components/JuntoComAGente/JuntoComAGente";
 import ComoFazerParte from "@/app/components/comoFazerParte/ComoFazerParte";
@@ -16,17 +16,18 @@ export default function Home() {
   return (
     <>
       <PaqHeader />
-      <UmFuturoInovadorDiversoComecaAqui/>
+      <UmFuturoInovadorDiversoComecaAqui />
       <ConectarETransformar />
       <PapelPAQ />
       <Impaqtrometro />
       <JuntoComAGente />
-      <NossoTime/>
+      <NossoTime />
+      <Depoimentos />
       <ComoPossoFazerParte />
       <ComoFazerParte />
       <Contact />
       <ToastContainer position="bottom-center" />
-      <Footer/>
+      <Footer />
     </>
   );
 }
