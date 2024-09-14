a2da9895b249574d62d5207b91a3858a55815fbf
Thiago Cerutti <thiagocerutti@gmail.com>
2024-09-14 16:38:05 -0300

    fix: uso de css modules

--- src/app/components/ProgramasEspacos/ProgramasEspacos.tsx
+++ src/app/components/ProgramasEspacos/ProgramasEspacos.tsx
@@ -8,61 +8,59 @@ import cardImgTwo from "./imgs/img_2.png"
 import cardImgThree from "./imgs/img_3.jpeg"
 import cardImgFour from "./imgs/img_4.png"
 
-import "./ProgramasEspacos.css"
-
+import styles from "./ProgramasEspacos.module.css";
 
 export default function ProgramasEspacos() {
   return (
-    <section className="program-spaces">
-      <div className="program-header">
-        <Image src={bookSvg} alt="Livro com óculos" id="bookIcon" />
-        <h2 className="mt-32" id="programs-title"><span className="font-bold">PROGRAMAS</span> <br /> E ESPAÇOS</h2>
-        <p id="program-header-text">
-          Nossa metodologia é a parte mais importante e é
-          com ela que conseguimos colocar no mundo o impacto que acreditamos.
+    <section className={styles.programSpaces}>
+      <div className={styles.programHeader}>
+        <Image src={bookSvg} alt="Livro com óculos" />
+        <div className={styles.contanierTituloPagina}>
+          <span className={styles.tituloProgramas}>PROGRAMAS</span>
+          <span className={styles.tituloEspacos}>E ESPAÇOS</span>
+        </div>
+        <p>
+          Nossa metodologia é a parte mais importante e é com ela que
+          conseguimos colocar no mundo o impacto que acreditamos.
         </p>
       </div>
 
-      <div className="program-cards">
+      <div className={styles.programCards}>
         <AccordionCard
           title="IMERSÃO"
           content="Primeiros passos para explorar as carreiras cultura do ecossistema de tecnologia,
-          desenvolver habilidades e competências básicas - Porta de entrada para quem quer 
+          desenvolver habilidades e competências básicas - Porta de entrada para quem quer
           iniciar no PAQ!"
           imageSrc={cardImgOne}
           alt="Jovens no PAQ"
         />
 
-
         <AccordionCard
           title="ACELERAÇÃO"
           content="Primeiros passos para explorar as carreiras cultura do ecossistema de tecnologia,
-          desenvolver habilidades e competências básicas - Porta de entrada para quem quer 
+          desenvolver habilidades e competências básicas - Porta de entrada para quem quer
           iniciar no PAQ!"
           imageSrc={cardImgTwo}
           alt="Jovens no PAQ"
         />
 
-
         <AccordionCard
           title="EXPERIÊNCIAS DE APRENDIZADO"
           content="Primeiros passos para explorar as carreiras cultura do ecossistema de tecnologia,
-          desenvolver habilidades e competências básicas - Porta de entrada para quem quer 
+          desenvolver habilidades e competências básicas - Porta de entrada para quem quer
           iniciar no PAQ!"
           imageSrc={cardImgThree}
           alt="Jovens no PAQ"
         />
 
-
         <AccordionCard
           title="CENTROS DE APRENDIZADO"
           content="Primeiros passos para explorar as carreiras cultura do ecossistema de tecnologia,
-          desenvolver habilidades e competências básicas - Porta de entrada para quem quer 
+          desenvolver habilidades e competências básicas - Porta de entrada para quem quer
           iniciar no PAQ!"
           imageSrc={cardImgFour}
           alt="Sala com computadores"
         />
-
       </div>
     </section>
   );
