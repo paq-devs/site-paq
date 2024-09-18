ea7166d6cdaf6ba1c0a2ba3f20e78a8b65143474
iofme <jgempregos02@gmail.com>
2024-09-18 13:51:04 -0300

    git pull na main

--- src/app/components/ConectarETransformar/VideoConexao.tsx
+++ src/app/components/ConectarETransformar/VideoConexao.tsx
@@ -5,11 +5,7 @@ export default function VideoConexao() {
   return (
     <div className={styles.videoContainer}>
       <iframe
-<<<<<<< HEAD
         src="https://www.youtube.com/embed/9y1dih3eeJA?si=NO0ahc6Gf2caZ_gf"
-=======
-        src="https://www.youtube.com/embed/9y1dih3eeJA"
->>>>>>> cc42d5734e2e2543d8544653e387f571314cdfd3
         allowFullScreen
       ></iframe>
     </div>
