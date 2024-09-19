ec9a48f99996c33071c083fc56c40f956ef58f6e
Thiago Cerutti <thiagocerutti@gmail.com>
2024-09-19 20:33:37 -0300

    feat: estilos de card de imersão

--- src/app/components/ProgramasEspacos/CardImersao/index.tsx
+++ src/app/components/ProgramasEspacos/CardImersao/index.tsx
@@ -1,5 +1,6 @@
 import MobileCard from "../MobileCard";
 import imersao from "../img/bg-imersao.png";
+import style from "./CardImersao.module.css";
 
 export default function CardImersao() {
   return (
@@ -8,19 +9,37 @@ export default function CardImersao() {
       imagem={imersao}
       descricaoCurta="Primeiros passos para explorar as carreiras cultura do ecossistema de tecnologia,  desenvolver habilidades e competências básicas - Porta de entrada para quem quer iniciar no PAQ!"
     >
-      <p>
-        Nosso movimento é de{" "}
-        <strong>
-          trazer as quebradas pra dentro dos centros de inovação e tecnologia
-        </strong>
-        , empoderando e mostrando que esses espaços podem e devem ser ocupados!
-      </p>
-
-      <p>
-        Para que isso aconteça precisamos garantir espaços com toda a estrutura
-        necessária para que os jovens possam se desenvolver com qualidade e
-        imersos nos mesmos ambientes que empresas inovadoras.
-      </p>
+      <div className={style.container}>
+        <p>
+          <strong>Conteúdo:</strong> Expanda a visão sobre mercado tech Conheça
+          as principais carreiras para se seguir na tech e troque ideia com
+          profissionais incríveis de diversas áreas.
+        </p>
+        <p>
+          <ul>
+            <li>
+              <strong>Informações Importantes:</strong>
+            </li>
+            <li>
+              <strong>Público:</strong> Jovens de 16 a 21 anos
+            </li>
+            <li>
+              <strong>Duração:</strong> 8 semanas
+            </li>
+            <li>
+              <strong>Formato:</strong> Presencial nas unidades Pedra Branca
+              (Palhoça) e SC-401 (Florianópolis)
+            </li>
+            <li>
+              <strong>Inscrições:</strong> trimestralmente (Site e instagram)
+            </li>
+            <li>
+              <strong>Pré-requisitos:</strong> Conhecimentos básicos de
+              informática
+            </li>
+          </ul>
+        </p>
+      </div>
     </MobileCard>
   );
 }
