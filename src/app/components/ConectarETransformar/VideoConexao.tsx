81207ac182235106e6b972308b75302741a2e3f7
João-Kuhn <117414638+iofme@users.noreply.github.com>
2024-09-19 17:20:24 -0300

    Programas e espaços (#46)
    * Arrumando o link do vídeo
    
    * git pull na main
    
    * Acordeom feito
    
    * Pronto pro ar

--- src/app/components/ConectarETransformar/VideoConexao.tsx
+++ src/app/components/ConectarETransformar/VideoConexao.tsx
@@ -5,7 +5,7 @@ export default function VideoConexao() {
   return (
     <div className={styles.videoContainer}>
       <iframe
-        src="https://www.youtube.com/embed/9y1dih3eeJA"
+        src="https://www.youtube.com/embed/9y1dih3eeJA?si=NO0ahc6Gf2caZ_gf"
         allowFullScreen
       ></iframe>
     </div>
