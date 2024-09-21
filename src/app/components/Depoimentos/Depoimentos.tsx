9d908130ef8794cc47b423e9d1041615760376ad
Tiago Pereira <tiagops.prog@gmail.com>
2024-09-20 21:04:10 -0300

    Corrige a responsividade e ajusta componente para o estilo do figma

--- src/app/components/Depoimentos/Depoimentos.tsx
+++ src/app/components/Depoimentos/Depoimentos.tsx
@@ -1,13 +1,14 @@
 "use client"
+import { useRef, useCallback } from "react";
 import CardDepoimento from "./CardDepoimento";
 import depoimentos from "./cards";
 import style from "./Depoimentos.module.css";
 import "./swiper.css"
 import Image from "next/image";
 import Medalha from "./img/Medalha.svg"
+import Arrow from './img/arrow.svg'
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperRef, SwiperSlide, SwiperSlideProps } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
@@ -15,6 +16,18 @@ import 'swiper/css';
 
 
 export default function Depoimentos() {
+  const sliderRef = useRef<SwiperRef>(null)
+  
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slidePrev();
+  }, []);
+
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slideNext();
+  }, []);
+
 
   return (
     <section id="historias" className={style.container}>
@@ -31,12 +44,22 @@ export default function Depoimentos() {
       </aside>
       <div className={style.carrossel}>
 
-        <Swiper className={style.mySwiper}
+        <Swiper 
+          ref={sliderRef}
+          className={style.mySwiper}
           loop={true}
-          modules={[Navigation, Pagination]}
-          slidesPerView={3}
-          centeredSlides={true}
-          navigation={true}
+          modules={[Pagination]}
+          spaceBetween={0}
+          slidesPerView={1}
+          breakpoints={{
+            1440: {
+              slidesPerView: 3
+            },
+            1024: {
+              slidesPerView: 2
+            }
+          }}
+          navigation={false}
           grabCursor={true}
           pagination={{
             clickable: true,
@@ -56,6 +79,13 @@ export default function Depoimentos() {
             </SwiperSlide>
           ))}
         </Swiper>
+        <div className={style.custom_arrows}>
+          <div onClick={handlePrev}>
+            <Image className={style.left_arrow}  src={Arrow} width={85.58} height={84} alt="Voltar carrossel" />
+          </div>
+          
+          <Image className={style.right_arrow} onClick={handleNext} src={Arrow} width={85.58} height={84} alt="Avançar carrossel" />
+        </div>
       </div>
     </section>
   );
