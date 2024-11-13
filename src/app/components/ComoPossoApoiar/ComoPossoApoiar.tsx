107551d6edfd59b4242501daf60751027c42c220
Thiago Cerutti <thiagocerutti@gmail.com>
2024-11-13 20:10:59 -0300

    fix: css do texto de apoio para doadores

--- src/app/components/ComoPossoApoiar/ComoPossoApoiar.tsx
+++ src/app/components/ComoPossoApoiar/ComoPossoApoiar.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 import CardComoPossoApoiar from "./CardsComoPossoApoiar";
 import check from './img/CheckBranco.png';
+import style from './ComoPossoApoiar.module.css';
 
 import './ComoPossoApoiar.css';
 
@@ -25,8 +26,8 @@ export default function ComoPossoFazerParte() {
       />
       <h2 className="ComoPossoApoiarTitulo mt-5"> como posso <br /><span className="font-bold">Apoiar?</span></h2>
       <CardComoPossoApoiar onSelectButton={handleButtonSelect} />
-      <div className="bg-[#00171F] w-auto py-10 px-12 rounded-[34px] mt-36 -mb-[90px] max-[768px]:px-10 max-[768px]:py-8 max-[768px]:mx-6 max-[768px]:-mb-[150px]">
-        <p className="ComoPossoApoiarTexto w-full">O apoio financeiro é essencial para a permanência da nossa <br/>
+      <div className={style.apoioFinanceiro}>
+        <p>O apoio financeiro é essencial para a permanência da nossa <br/>
         missão no mundo, mas existem diversas formas de fortalecer a <br/>
         nossa missão e ser parte da construção de um futuro mais <br/>
         diverso e realmente inovador.
