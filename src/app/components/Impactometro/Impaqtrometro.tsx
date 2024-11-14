08d2e2e8d765fd27337104a39d2c11afe1ea8ac4
Thiago Cerutti <thiagocerutti@gmail.com>
2024-11-14 20:59:45 -0300

    feat: adiciona seção com links do footer da deção (#91)

--- src/app/components/Impactometro/Impaqtrometro.tsx
+++ src/app/components/Impactometro/Impaqtrometro.tsx
@@ -12,117 +12,141 @@ import Tedd from "./img/Tedd.png";
 import william from "./img/William.png";
 import suanam from "./img/suanamecaio.png";
 import formados from './img/grupoDePessoasFormadas.png'
+import styles from "./impaqtrometro.module.css";
 
-import './impaqtrometro.css'
-
+console.log(styles.ImpaqtrometroTitulo);
 
 export default function Impaqtrometro() {
   return (
-    <section id="impaqtometro" className="w-full flex flex-col items-center rounded-[20px] bg-[#00171F] p-[53px_33px] pb-56">
-      <GlobalStyle />
-      <div className="flex flex-col items-center gap-7 mt-12 mb-16 lg:mt-36">
-        <Image src={imagemRelogio} alt="" width={75} height={75} />
-        <h2 className="ImpaqtrometroTitulo">
-          <span className="font-bold">IM<span className="text-azulpaq">PAQ</span>TÔMETRO:</span><br />
-          O IMPACTO REAL
-        </h2>
-      </div>
-      <div className="flex flex-col gap-3 mb-20">
-        <div className="flex items-center gap-6">
-          <CardImpaqtrometro
-            image={grupoDePessoas}
-            numero="80"
-            titulo="jovens"
-            descricao="Ativos na comunidade de aprendizagem"
-          />
-          <Image
-            src={william}
-            alt=""
-            width={403}
-            height={344}
-            className="hidden lg:block aspect-auto object-contain rounded-[40px]"
-          />
-           <Image
-            src={william}
-            alt=""
-            width={244}
-            height={208}
-            className="hidden md:block aspect-auto object-contain  rounded-[40px] lg:hidden"
-          />
+    <div className={styles.container}>
+      <section id="impaqtometro">
+        <GlobalStyle />
+        <div className="flex flex-col items-center gap-7 mt-12 mb-16 lg:mt-36">
+          <Image src={imagemRelogio} alt="" width={75} height={75} />
+          <h2 className={styles.ImpaqtrometroTitulo}>
+            <span className="font-bold">
+              IM<span className="text-azulpaq">PAQ</span>TÔMETRO:
+            </span>
+            <br />O IMPACTO REAL
+          </h2>
         </div>
-        <div className="flex items-center gap-6">
-          <Image
-            src={Tedd}
-            alt=""
-            width={403}
-            height={344}
-            className="hidden lg:block aspect-auto object-contain rounded-[40px]"
-          />
-           <Image
-            src={Tedd}
-            alt=""
-            width={244}
-            height={208}
-            className="hidden md:block aspect-auto object-contain  rounded-[40px] lg:hidden"
-          />
-          <CardImpaqtrometro
-            image={formados}
-            numero="200"
-            titulo="jovens"
-            descricao="Atendidos em nossos programas"
-          />
+        <div className="flex flex-col gap-3 mb-20">
+          <div className="flex items-center gap-6">
+            <CardImpaqtrometro
+              image={grupoDePessoas}
+              numero="80"
+              titulo="jovens"
+              descricao="Ativos na comunidade de aprendizagem"
+            />
+            <Image
+              src={william}
+              alt=""
+              width={403}
+              height={344}
+              className="hidden lg:block aspect-auto object-contain rounded-[40px]"
+            />
+            <Image
+              src={william}
+              alt=""
+              width={244}
+              height={208}
+              className="hidden md:block aspect-auto object-contain  rounded-[40px] lg:hidden"
+            />
+          </div>
+          <div className="flex items-center gap-6">
+            <Image
+              src={Tedd}
+              alt=""
+              width={403}
+              height={344}
+              className="hidden lg:block aspect-auto object-contain rounded-[40px]"
+            />
+            <Image
+              src={Tedd}
+              alt=""
+              width={244}
+              height={208}
+              className="hidden md:block aspect-auto object-contain  rounded-[40px] lg:hidden"
+            />
+            <CardImpaqtrometro
+              image={formados}
+              numero="200"
+              titulo="jovens"
+              descricao="Atendidos em nossos programas"
+            />
+          </div>
+          <div className="flex items-center gap-6">
+            <CardImpaqtrometro
+              image={voluntariado}
+              numero="40"
+              titulo="jovens"
+              descricao="Trabalhando em empresas de tecnologia"
+            />
+            <Image
+              src={anajulia}
+              alt=""
+              width={403}
+              height={344}
+              className="hidden lg:block aspect-auto object-contain  rounded-[40px]"
+            />
+            <Image
+              src={anajulia}
+              alt=""
+              width={244}
+              height={208}
+              className="hidden md:block aspect-auto object-contain  rounded-[40px] lg:hidden"
+            />
+          </div>
+          <div className="flex items-center gap-6">
+            <Image
+              src={suanam}
+              alt=""
+              width={403}
+              height={344}
+              className="hidden lg:block aspect-auto object-contain  rounded-[40px]"
+            />
+            <Image
+              src={suanam}
+              alt=""
+              width={244}
+              height={208}
+              className="hidden md:block aspect-auto object-contain  rounded-[40px] lg:hidden"
+            />
+            <CardImpaqtrometro
+              image={investidor}
+              numero="780mil"
+              titulo="reais"
+              descricao="injetados nas quebradas através dos jovens"
+            />
+          </div>
         </div>
-        <div className="flex items-center gap-6">
-          <CardImpaqtrometro
-            image={voluntariado}
-            numero="40"
-            titulo="jovens"
-            descricao="Trabalhando em empresas de tecnologia"
-          />
-          <Image
-            src={anajulia}
-            alt=""
-            width={403}
-            height={344}
-            className="hidden lg:block aspect-auto object-contain  rounded-[40px]"
-          />
-           <Image
-            src={anajulia}
-            alt=""
-            width={244}
-            height={208}
-            className="hidden md:block aspect-auto object-contain  rounded-[40px] lg:hidden"
-          />
-        </div>
-        <div className="flex items-center gap-6">
-          <Image
-            src={suanam}
-            alt=""
-            width={403}
-            height={344}
-            className="hidden lg:block aspect-auto object-contain  rounded-[40px]"
-          />
-          <Image
-            src={suanam}
-            alt=""
-            width={244}
-            height={208}
-            className="hidden md:block aspect-auto object-contain  rounded-[40px] lg:hidden"
-          />
-          <CardImpaqtrometro
-            image={investidor}
-            numero="780mil"
-            titulo="reais"
-            descricao="injetados nas quebradas através dos jovens"
-          />
-        </div>
-      </div>
-      <div className="w-64 md:w-[498px]">
-        <p className="ImpaqtrometroTexto mb-9">
-        Clique no botão abaixo para acessar os relatórios institucionais dos últimos anos e ver detalhes:
-        </p>
-      </div>
-      <Button title="VER RELATÓRIOS" href="https://drive.google.com/drive/folders/1BWlhJdLAxDiH627k0KvBmNHLD5hKT3fe" width="150px" height="42px"/>
-    </section>
+        <footer>
+          <h2>portal da transparência</h2>
+          <div>
+            <Button
+              title="VER RELATÓRIOS"
+              href="https://drive.google.com/drive/folders/137Qh9c7aQejgg9IDlzIVxlFNaneUbSKK?usp=sharing"
+              width="150px"
+              height="42px"
+              newTab
+            />
+            <Button
+              title="ESTATUTO"
+              href="https://drive.google.com/drive/folders/11j-IDR46-vxoSy7hgWb5tOYXqPE7BJ0P?usp=drive_link"
+              width="150px"
+              height="42px"
+              newTab
+            />
+            <Button
+              title="BALANCETE"
+              href="https://drive.google.com/drive/folders/11NDYCP33CkR79YR7kY4ZwfQQC_YVwPVI?usp=sharing"
+              width="150px"
+              height="42px"
+              newTab
+            />
+          </div>
+        </footer>
+      </section>
+    </div>
   );
 }
