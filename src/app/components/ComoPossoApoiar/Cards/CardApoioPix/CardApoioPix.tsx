372d5adeb791c6ad6018ebb65a7b028927db831d
Thiago Cerutti <thiagocerutti@gmail.com>
2024-11-15 19:03:25 -0300

    feat: versão mobile do apoio via PIX

--- src/app/components/ComoPossoApoiar/Cards/CardApoioPix/CardApoioPix.tsx
+++ src/app/components/ComoPossoApoiar/Cards/CardApoioPix/CardApoioPix.tsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 import Image from "next/image";
 import apoioPix from "./img/apoio-pix.png";
 import pixQrcode from "./img/pix-qrcode.png";
+import apoieNoPixMobile from "./img/apoie-pix-mobile.png";
 import styles from "./CardApoioPix.module.css";
 
 const CardApoioPix = () => {
   return (
     <div className={styles.container}>
       <div className={styles.containerText}>
-        <Image src={apoioPix} alt="Apoio via PIX" width={263} height={192} />
+        <Image src={apoioPix} alt="Apoio via PIX" width={263} height={192} className={styles.imgDesktop} />
+        <Image src={apoieNoPixMobile} alt="Apoie via PIX" width={263} height={192} className={styles.imgMobile} />
         <p>
           Quer dar aquela força mas sem compromisso? Manda um pix pra gente!
           Qualquer valor ajuda e faz toda a diferença!
