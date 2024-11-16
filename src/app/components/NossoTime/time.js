7c6fa964cb2de29738b7e78795791c25a16dd5e4
Thiago Cerutti <thiagocerutti@gmail.com>
2024-11-15 23:12:22 -0300

    fix: clean up de assets não utilizados

--- src/app/components/NossoTime/time.js
+++ src/app/components/NossoTime/time.js
@@ -2,16 +2,11 @@ import yohana from "./img/diretoria/yohanna-nosso-time.png";
 import jeff from "./img/diretoria/jeff.png";
 import ted from "./img/diretoria/ted.png";
 
-import cadu from "./img/cadu.png";
+import cadu from "./img/equipe/cadu.png";
 import fernando from "./img/equipe/fernando.png";
 import pietra from "./img/equipe/pietra-nosso-time.png";
 import duda from "./img/equipe/duda-aguiar.png";
 import leonardo from "./img/equipe/leonardo-martins.png";
-import bow from "./img/bow.png";
-import marilia from "./img/marilia.png";
-import pompeu from "./img/pompeu.png";
-import igor from "./img/igor.png";
-import evanz from "./img/evanz.png";
 
 import moacirMarafon from "./img/conselho/moacir-marafon.png";
 import aliceCarneiro from "./img/conselho/alice-carneiro.png";
@@ -22,23 +17,6 @@ import eliezerLeal from "./img/conselho/eliezer-leal.png";
 import felipeMandavalli from "./img/conselho/felipe-mandavalli.png";
 import talitaMatos from "./img/conselho/talita-matos.png";
 import thiaraGaldinho from "./img/conselho/thiara-galdinho.png";
-
-import cerutti from "./img/cerutti.png";
-import thaysa from "./img/thaysa.png";
-import welington from "./img/welington.png";
-import layse from "./img/layse.png";
-import euler from "./img/euler.png";
-import nathalia from "./img/bow.png";
-import hugo from "./img/hugo.png";
-import ariana from "./img/ariana.png";
-import waltao from "./img/waltao.png";
-import dafne from "./img/dafne.png";
-import solange from "./img/solange.png";
-import kleber from "./img/kleber.png";
-import felipe from "./img/felipe.png";
-import mariasilveira from "./img/mariaSilveira.png";
-import miguel from "./img/miguel.png";
-import sahra from "./img/sahra.png";
 
 export const Diretoria = [
   { src: jeff, nome: "Jefferson Lima", cargo: "Presidente" },
@@ -62,32 +40,45 @@ export const Equipe = [
 ];
 
 export const Conselhos = [
-  { src: moacirMarafon, nome: "Moacir Marafon.png", cargo: "Conselho Deliberativo" },
-  { src: cristianeIata, nome: "Cristiane Iata.png", cargo: "Conselho Deliberativo" },
-  { src: felipeMandavalli, nome: "Felipe Mandavalli.png", cargo: "Conselho Deliberativo" },
-  { src: diegoChierighini, nome: "Diego Chierighini.png", cargo: "Conselho Deliberativo" },
-  { src: aliceCarneiro, nome: "Alice Carneiro.png", cargo: "Conselho Deliberativo" },
-  { src: eliezerLeal, nome: "Eliezer Leal.png", cargo: "Conselho Deliberativo" },
-  { src: talitaMatos, nome: "Talita Matos.png", cargo: "Conselho Deliberativo" },
-  { src: thiaraGaldinho, nome: "Thiara Galdinho.png", cargo: "Conselho Deliberativo" },
+  {
+    src: moacirMarafon,
+    nome: "Moacir Marafon.png",
+    cargo: "Conselho Deliberativo",
+  },
+  {
+    src: cristianeIata,
+    nome: "Cristiane Iata.png",
+    cargo: "Conselho Deliberativo",
+  },
+  {
+    src: felipeMandavalli,
+    nome: "Felipe Mandavalli.png",
+    cargo: "Conselho Deliberativo",
+  },
+  {
+    src: diegoChierighini,
+    nome: "Diego Chierighini.png",
+    cargo: "Conselho Deliberativo",
+  },
+  {
+    src: aliceCarneiro,
+    nome: "Alice Carneiro.png",
+    cargo: "Conselho Deliberativo",
+  },
+  {
+    src: eliezerLeal,
+    nome: "Eliezer Leal.png",
+    cargo: "Conselho Deliberativo",
+  },
+  {
+    src: talitaMatos,
+    nome: "Talita Matos.png",
+    cargo: "Conselho Deliberativo",
+  },
+  {
+    src: thiaraGaldinho,
+    nome: "Thiara Galdinho.png",
+    cargo: "Conselho Deliberativo",
+  },
   { src: andreRocha, nome: "Andre Rocha.png", cargo: "Conselho Fiscal" },
-];
-
-export const Mentors = [
-  { src: cerutti, nome: "Thiago Cerutti", cargo: "PROGRAMAÇÃO" },
-  { src: thaysa, nome: "Thaysa Lacerda", cargo: "PROGRAMAÇÃO" },
-  { src: welington, nome: "Wellington Rodrigues", cargo: "PROGRAMAÇÃO" },
-  { src: layse, nome: "Layse Gabrielly", cargo: "PROGRAMAÇÃO" },
-  { src: euler, nome: "Euler Filho", cargo: "PROGRAMAÇÃO" },
-  { src: nathalia, nome: " Nathália Cechetti ", cargo: "DESIGN" },
-  { src: hugo, nome: "Hugo Raniere", cargo: "DESIGN" },
-  { src: ariana, nome: "Ariana Regina", cargo: "DESIGN" },
-  { src: waltao, nome: "Waltão", cargo: "DESIGN" },
-  { src: dafne, nome: "Dafne Miranda", cargo: "DESIGN" },
-  { src: solange, nome: "Solange Gamboa", cargo: "PROGRAMAÇÃO" },
-  { src: kleber, nome: "Kleber Furtado", cargo: "PROGRAMAÇÃO" },
-  { src: felipe, nome: "Felipe Flores", cargo: "PROGRAMAÇÃO" },
-  { src: mariasilveira, nome: "maria silveira", cargo: "DESIGN" },
-  { src: miguel, nome: "Miguel Matos", cargo: "DESIGN" },
-  { src: sahra, nome: "Sahra Di Bernardi", cargo: "PROGRAMAÇÃO" },
 ];
