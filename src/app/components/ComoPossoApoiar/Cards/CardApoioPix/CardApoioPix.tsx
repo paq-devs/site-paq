58e1b0c72e70f7819242902e82b57a4c54e54ff6
Thiago Cerutti <thiagocerutti@gmail.com>
2024-11-16 08:22:13 -0300

    fix: typo

--- src/app/components/ComoPossoApoiar/Cards/CardApoioPix/CardApoioPix.tsx
+++ src/app/components/ComoPossoApoiar/Cards/CardApoioPix/CardApoioPix.tsx
@@ -15,7 +15,7 @@ const CardApoioPix = () => {
           Quer dar aquela força mas sem compromisso? Manda um pix pra gente!
           Qualquer valor ajuda e faz toda a diferença!
         </p>
-        <span>admnistrativo.paq@prototipandoaquebrada.org</span>
+        <span>administrativo.paq@prototipandoaquebrada.org</span>
       </div>
       <div className={styles.containerImage}>
         <Image src={pixQrcode} alt="QR Code PIX" width={450} height={526} />
