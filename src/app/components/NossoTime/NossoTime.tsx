5ae0fa2abe2d157800b5253cddd2070b18122950
Thiago Cerutti <thiagocerutti@gmail.com>
2024-11-16 08:15:51 -0300

    Alterar seção "Nosso time" (#96)
    * feat: adiciona imagens do time
    
    * fix: clean up de assets não utilizados
    
    * fix: nomes
    
    * fix: responsividade
    
    * refactor: de Tailwind para css nativo
    
    * fix: margin

--- src/app/components/NossoTime/NossoTime.tsx
+++ src/app/components/NossoTime/NossoTime.tsx
@@ -1,21 +1,15 @@
 import Image from "next/image";
 import ferramenta from "./img/Ferramenta.svg";
-import style from "./NossoTime.module.css";
+import styles from "./NossoTime.module.css";
 import NossoTimeCarrosel from "./NossoTimeCarrosel";
 
 export default function NossoTime() {
   return (
-    <section className="flex flex-col items-center">
-      <div className="flex flex-col items-center gap-6">
-        <Image src={ferramenta} width={74} height={74} alt="" />
-        <p className={style.NossoTimeTitulo}>
-          <span className="font-bold">nosso time:</span> linha de frente
-        </p>
-        <p className={style.NossoTimeParagrafo}>
-          O PAQ é construído diariamente por um time incrível totalmente
-          dedicado para a missão:
-        </p>
-      </div>
+    <section className={styles.container}>
+      <Image src={ferramenta} width={74} height={74} alt="" />
+      <p className={styles.NossoTimeTitulo}>
+        <strong>nosso time:</strong> linha de frente
+      </p>
       <NossoTimeCarrosel />
     </section>
   );
--- src/app/components/NossoTime/NossoTimeCarrosel.tsx
+++ src/app/components/NossoTime/NossoTimeCarrosel.tsx
@@ -2,56 +2,67 @@
 "use client";
 import { useState, ReactNode } from 'react';
 import Image from "next/image";
-import { Staff, Mentors } from './time';
-import style from './NossoTime.module.css'
+import { Diretoria, Equipe, Conselhos } from "./time";
+import styles from "./NossoTimeCarrosel.module.css";
 
-const Button: React.FC<{children: ReactNode, onClick: Function, selected: boolean}> = ({ children, onClick, selected }) => {
+const Button: React.FC<{
+  children: ReactNode;
+  onClick: Function;
+  selected: boolean;
+}> = ({ children, onClick, selected }) => {
   return (
     <button
       onClick={() => onClick()}
-      className={`font-poppins px-4 md:px-7 py-2 font-bold transition-all duration-500 ease-in-out ${selected ? 'bg-azulopcoes text-white rounded-[40px]  my-1' : ' text-black rounded-[40px] my-1 '}`}
+      className={`${styles.botaoNavegacao} ${
+        selected
+          ? styles.botaoNavegacaoSelecionado
+          : styles.botaoNavegacaoNaoSelecionado
+      }`}
     >
       {children}
     </button>
-  )
-}
+  );
+};
 
 const NossoTimeCarrosel: React.FC = () => {
   const [selectedButton, setSelectedButton] = useState<number>(0);
-  const team = [Staff, Mentors]
+  const team = [Diretoria, Equipe, Conselhos];
 
   return (
-    <div className="max-w-full flex flex-col items-center mt-9">
-      <div className="flex space-x-4 mb-8 lg:bg-fundinhocinza rounded-[40px] px-3 md:bg-white bg-fundocinza">
+    <div className={styles.container}>
+      <nav>
         <Button
           selected={selectedButton == 0}
           onClick={() => setSelectedButton(0)}
         >
-          Time
+          Diretorias
         </Button>
         <Button
           selected={selectedButton == 1}
           onClick={() => setSelectedButton(1)}
         >
-          Mentores
+          Equipe
         </Button>
-      </div>
-      <div className="flex max-w-full pb-8 gap-y-8 p-4 items-center overflow-x-auto 2xl:overflow-none 2xl:flex-wrap 2xl:justify-center">
-        {
-          team[selectedButton].map((member, index) => (
-            <div key={index} className="mx-6 bg-fundocinza flex-none rounded-[27px] first:ml-16 2xl:first:ml-6">
-              <Image
-                src={member.src}
-                alt={`Imagem ${index + 1}`}
-                width={298}
-                height={229}
-                className="aspect-auto rounded-[27px]"
-              />
-              <h2 className={style.CardsNossoTimeNome}>{member.nome}</h2>
-              <p className={style.CardsNossoTimeCargo}>{member.cargo}</p>
-            </div>
-          ))
-        }
+        <Button
+          selected={selectedButton == 2}
+          onClick={() => setSelectedButton(2)}
+        >
+          Conselhos
+        </Button>
+      </nav>
+      <div className={styles.containerTime}>
+        {team[selectedButton].map((member, index) => (
+          <div key={index} className={styles.cardPessoa}>
+            <Image
+              src={member.src}
+              alt={`Imagem ${index + 1}`}
+              width={298}
+              height={229}
+            />
+            <h2 className={styles.CardsNossoTimeNome}>{member.nome}</h2>
+            <p className={styles.CardsNossoTimeCargo}>{member.cargo}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
