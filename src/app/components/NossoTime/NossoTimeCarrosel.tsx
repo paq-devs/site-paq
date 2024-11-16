4948d6ff1ecbb48c8f9c58f7aeb119a063faffb7
Thiago Cerutti <thiagocerutti@gmail.com>
2024-11-15 23:07:36 -0300

    feat: adiciona imagens do time

--- src/app/components/NossoTime/NossoTimeCarrosel.tsx
+++ src/app/components/NossoTime/NossoTimeCarrosel.tsx
@@ -2,7 +2,7 @@
 "use client";
 import { useState, ReactNode } from 'react';
 import Image from "next/image";
-import { Staff, Mentors } from './time';
+import { Diretoria, Equipe, Conselhos } from "./time";
 import style from './NossoTime.module.css'
 
 const Button: React.FC<{children: ReactNode, onClick: Function, selected: boolean}> = ({ children, onClick, selected }) => {
@@ -18,7 +18,7 @@ const Button: React.FC<{children: ReactNode, onClick: Function, selected: boolea
 
 const NossoTimeCarrosel: React.FC = () => {
   const [selectedButton, setSelectedButton] = useState<number>(0);
-  const team = [Staff, Mentors]
+  const team = [Diretoria, Equipe, Conselhos];
 
   return (
     <div className="max-w-full flex flex-col items-center mt-9">
@@ -27,31 +27,38 @@ const NossoTimeCarrosel: React.FC = () => {
           selected={selectedButton == 0}
           onClick={() => setSelectedButton(0)}
         >
-          Time
+          Diretorias
         </Button>
         <Button
           selected={selectedButton == 1}
           onClick={() => setSelectedButton(1)}
         >
-          Mentores
+          Equipe
+        </Button>
+        <Button
+          selected={selectedButton == 2}
+          onClick={() => setSelectedButton(2)}
+        >
+          Conselhos
         </Button>
       </div>
       <div className="flex max-w-full pb-8 gap-y-8 p-4 items-center overflow-x-auto 2xl:overflow-none 2xl:flex-wrap 2xl:justify-center">
-        {
-          team[selectedButton].map((member, index) => (
-            <div key={index} className="mx-6 bg-fundocinza flex-none rounded-[27px] first:ml-16 2xl:first:ml-6">
-              <Image
-                src={member.src}
-                alt={`Imagem ${index + 1}`}
-                width={298}
-                height={229}
-                className="aspect-auto rounded-[27px]"
-              />
-              <h2 className={style.CardsNossoTimeNome}>{member.nome}</h2>
-              <p className={style.CardsNossoTimeCargo}>{member.cargo}</p>
-            </div>
-          ))
-        }
+        {team[selectedButton].map((member, index) => (
+          <div
+            key={index}
+            className="mx-6 bg-fundocinza flex-none rounded-[27px] first:ml-16 2xl:first:ml-6"
+          >
+            <Image
+              src={member.src}
+              alt={`Imagem ${index + 1}`}
+              width={298}
+              height={229}
+              className="aspect-auto rounded-[27px]"
+            />
+            <h2 className={style.CardsNossoTimeNome}>{member.nome}</h2>
+            <p className={style.CardsNossoTimeCargo}>{member.cargo}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
