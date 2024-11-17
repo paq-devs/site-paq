ea3e3af7e79cbaaab878dfead48ee85095011adb
Thiago Cerutti <thiagocerutti@gmail.com>
2024-11-17 14:47:34 -0300

    feat: replace `react-slick` with custom component

--- src/app/components/UmFuturoDiversoDesktop/index.tsx
+++ src/app/components/UmFuturoDiversoDesktop/index.tsx
@@ -2,7 +2,7 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import React from "react";
-import Slider from "react-slick";
+import Slider from "../Slider";
 import Image from "next/image";
 
 import style from "./UmFuturoDiversoDesktop.module.css";
@@ -12,28 +12,12 @@ import img02 from "./img/carrossel-02-min.png";
 import img03 from "./img/carrossel-03-min.png";
 
 export default function UmFuturoDiversoDesktop() {
-  var settings = {
-    dots: false,
-    arrows: false,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true,
-  };
   return (
     <div className={style.containerDesktop}>
-      <Slider {...settings} className={style.container}>
-        <div>
-          <Image src={img01} alt="a" width={1024} height={466} />
-        </div>
-        <div>
-          <Image src={img02} alt="a" width={1024} height={466} />
-        </div>
-        <div>
-          <Image src={img03} alt="a" width={1024} height={466} />
-        </div>
+      <Slider intervalo={1500} movimento={500}>
+        <Image src={img01} alt="a" width={1024} height={466} />
+        <Image src={img02} alt="a" width={1024} height={466} />
+        <Image src={img03} alt="a" width={1024} height={466} />
       </Slider>
       <div className={style.containerFrase}>
         <span className={style.fraseUmFuturoDiverso}>
